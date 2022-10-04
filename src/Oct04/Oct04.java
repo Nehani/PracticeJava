7506be6cef5c0f90226be2205955a6025665b4bb
Nehani Gupta <nehani.gupta@gmail.com>
2022-10-04 18:20:39 +0530

    October 04 2022

---
+++ src/Oct04/Oct04.java
@@ -0,0 +1,13 @@
+package Oct04;
+
+public class Oct04 {
+
+	void getStatus() {
+		System.out.println("She loves you.");
+	}
+	public static void main(String[] args) {
+		System.out.println("Hi Jaan");
+		Oct04 obj = new Oct04();
+		obj.getStatus();
+	}
+}
